9ffdd8660b9ca988b985e808b99531f50f26519e
Felipe Sanches <sanchesfelipe153@gmail.com>
2021-03-07 19:51:06 -0300

    Removing invalid import from android module

--- client/android/src/main/kotlin/com/staticsanches/burger/builder/android/MainActivity.kt
+++ client/android/src/main/kotlin/com/staticsanches/burger/builder/android/MainActivity.kt
@@ -2,7 +2,6 @@ package com.staticsanches.burger.builder.android
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.datetime.Clock
 
 class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
